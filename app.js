9ca1db2cc82de4a0236c05416345ba595de35870
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2024-10-21 00:09:00 +0400

    make few changes

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const change = document.getElementById("dice-container");
 const adviceIndex = document.getElementById("index");
 const quote = document.getElementById("quote");
 
+async function information() {
+    const info = await fetch("https://api.adviceslip.com/advice");
+    const advice = await info.json();
+    console.log(advice);
+    
+    adviceIndex.innerHTML = `ADVICE #${advice.slip.id}`;
+    quote.innerHTML = `${advice.slip.advice}`
+}
+
+information();
+
 change.addEventListener("click", (event) => {
     async function information() {
         const info = await fetch("https://api.adviceslip.com/advice");
